e9ff1597d4daaa454b60e3109f837290430e2954
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2022-12-19 23:30:33 -0300

    fixing download

--- src/application/controllers/youtube/download-video.controller.ts
+++ src/application/controllers/youtube/download-video.controller.ts
@@ -11,28 +11,23 @@ import { ContainersEnum } from "../../../common/containers/containers.enum";
 @injectable()
 class DownloadVideoController implements Controller {
 	public handle(req: Request, res: Response) {
-		try {
-			const downloadVideoService: DownloadVideoServiceInterface =
-				container.resolve(ContainersEnum.DOWNLOAD_VIDEO_SERVICE);
+		const downloadVideoService: DownloadVideoServiceInterface =
+			container.resolve(ContainersEnum.DOWNLOAD_VIDEO_SERVICE);
 
-			const url = req.query.url as string;
-			res.header("Content-Disposition", "attachment; filename=video.mp4");
+		const url = req.query.url as string;
+		res.header("Content-Disposition", "attachment; filename=video.mp4");
 
-			const stream = downloadVideoService.downloadVideo(url);
+		const stream = downloadVideoService.downloadVideo(url);
 
-			return stream
-				.on("error", (error) => {
-					console.error("Stream Error:", error);
+		return stream
+			.on("error", (error) => {
+				console.error("Stream Error:", error);
 
-					return res
-						.status(400)
-						.json({ message: "Ops, algo deu errado" });
-				})
-				.pipe(res);
-		} catch (error: any) {
-			console.error(error);
-			return res.status(400).json({ message: "Ops, algo deu errado" });
-		}
+				return res
+					.status(400)
+					.json({ message: "Ops, algo deu errado" });
+			})
+			.pipe(res);
 	}
 }
 
--- src/infra/express/adapters/route-adapter.ts
+++ src/infra/express/adapters/route-adapter.ts
@@ -4,35 +4,27 @@ import { Controller } from "../../../application/controllers/controller";
 
 type Adapter = <T>(controller: Controller<T>) => RequestHandler;
 
-export const expressRouteAdapter: Adapter =
-	(controller) => async (req, res) => {
-		try {
-			console.log(
-				`METHOD: ${req.method} - URL: ${
-					req.originalUrl
-				} - IP_REQUEST: ${
-					req.headers["x-forwarded-for"] || req.socket.remoteAddress
-				} - HOST: ${
-					req.hostname
-				} - DATE: ${new Date().toLocaleString()}`
-			);
-			const { body, query, params, headers } = req;
+export const expressRouteAdapter: Adapter = (controller) => (req, res) => {
+	try {
+		console.log(
+			`METHOD: ${req.method} - URL: ${req.originalUrl} - IP_REQUEST: ${
+				req.headers["x-forwarded-for"] || req.socket.remoteAddress
+			} - HOST: ${req.hostname} - DATE: ${new Date().toLocaleString()}`
+		);
 
-			const { statusCode, data } = await controller.handle(req, res);
+		const data = controller.handle(req, res);
+		return data;
+	} catch (error) {
+		console.error(error as string);
 
-			res.status(statusCode).json(data);
-		} catch (error) {
-			console.error(error as string);
+		const defaultName = "ServerError";
+		const defaultMessage = "Server Error";
 
-			const defaultName = "ServerError";
-			const defaultMessage = "Server Error";
+		const { message = defaultMessage, name = defaultName } = error as any;
 
-			const { message = defaultMessage, name = defaultName } =
-				error as any;
-
-			res.status((error as any)?.statusCode ?? 500).json({
-				name,
-				message,
-			});
-		}
-	};
+		res.status((error as any)?.statusCode ?? 500).json({
+			name,
+			message,
+		});
+	}
+};
