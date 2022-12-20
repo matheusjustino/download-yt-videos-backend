ed22d7085c60edd6df9ff159431c535c71a7a0ac
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2022-12-19 22:17:06 -0300

    adding libs

--- src/infra/express/middleware.ts
+++ src/infra/express/middleware.ts
@@ -1,7 +1,11 @@
 import { Express, json } from "express";
 import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
 
 export const setupMiddleware = (app: Express): void => {
 	app.use(cors());
 	app.use(json());
+	app.use(helmet());
+	app.use(compression());
 };
