1342ed8994fdcb360032a78a0caa6215b96a01ed
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2022-12-19 22:45:28 -0300

    updating

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sobre a aplicação
+
+    Aplicação feita com NodeJs e Express que fornece uma API de download de vídeos do Youtube.
+
+## Frontend
+
+[Click aqui para visualizar o repositório do frontend.](https://github.com/matheusjustino/download-yt-videos-frontend)
--- src/infra/express/middleware.ts
+++ src/infra/express/middleware.ts
@@ -4,7 +4,11 @@ import helmet from "helmet";
 import compression from "compression";
 
 export const setupMiddleware = (app: Express): void => {
-	app.use(cors());
+	app.use(
+		cors({
+			origin: process.env.FRONTEND_URLS?.split(","),
+		})
+	);
 	app.use(json());
 	app.use(helmet());
 	app.use(compression());
